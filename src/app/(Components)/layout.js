00a11cd9843380cd47b0d17f9e0c4144eaf18ca0
jademat <01088049128a@gmail.com>
2025-04-07 15:25:07 +0900

    학생 리스트, 상세정보 폼 생성

--- src/app/(Components)/layout.js
+++ src/app/(Components)/layout.js
@@ -1,10 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import Header from '@/app/admin/layout/Header';
-import Navbar from '@/app/admin/layout/Navbar';
-import Footer from '@/app/admin/layout/Footer';
-import '@/app/admin/css/layout.css'; // 전역 레이아웃 CSS
+import Header from '@/app/(Components)/layout/Header.jsx';
+import Navbar from '@/app/(Components)/layout/Navbar.jsx';
+import Footer from '@/app/(Components)/layout/Footer.jsx';
+import '@/app/(Components)/css/layout.css'; // 전역 레이아웃 CSS
 
 export default function AdminLayout({ children }) {
     const [navbarOpen, setNavbarOpen] = useState(false);
